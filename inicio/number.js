e5c62d317d832f8a9b0856e0ff1bee322d54b4d1
theLucasb <lucasrodrigodes@gmail.com>
2026-04-27 21:47:54 -0300

    Fundamentos de JavaScript

---
+++ inicio/number.js
@@ -0,0 +1,8 @@
+let idade = 24;
+let altura = 1.76;
+
+console.log(idade);
+console.log(altura);
+
+console.log(idade + altura); // Somar
+console.log(idade, altura); // Concatenar
---
+++ inicio/templateString.js
@@ -0,0 +1,9 @@
+let nome = "Lucas";
+// String concatenada com variável
+let frase = "Olá " + nome + ", \nseja bem-vindo!";
+console.log(frase);
+
+// Template String
+frase = `Olá ${nome}, 
+seja bem-vindo!`;
+console.log(frase); 
