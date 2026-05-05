c517431fc6cd8e870b7cdbbf5261b7c8b6b41cb3
theLucasb <lucasrodrigodes@gmail.com>
2026-05-04 21:54:30 -0300

    Operadores em JS

---
+++ operadores/desafioOperadores.js
@@ -0,0 +1,20 @@
+// DESAFIO
+
+/* 
+    Atalho: SHIFT + ALT + A
+
+01. Utilizando os operadores relacionais, declare 2 variáveis e 
+imprima no output/console 5 valores TRUE e 5 valores FALSE.
+
+*/
+
+// Declare 2 variáveis
+let a = 10;
+let b = 20;
+
+// Valores TRUE
+
+
+// Valores FALSE
+
+
---
+++ operadores/logicos.js
@@ -0,0 +1,14 @@
+let temDinheiro = false;
+let estaChovendo = false;
+let carroEstaNaGaragem = true;
+
+let logicaAnd = "#AND Você vai ao cinema?";
+logicaAnd += temDinheiro && estaChovendo;
+console.log(logicaAnd);
+
+let logicaOr = "#OR Você vai ao cinema?";
+logicaOr += estaChovendo || carroEstaNaGaragem;
+console.log(logicaOr);
+
+
+
