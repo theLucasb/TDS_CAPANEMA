88bff51a0746b624c32185746ccaf53def6609a3
theLucasb <lucasrodrigodes@gmail.com>
2026-05-18 21:45:20 -0300

    Array, objetos e métodos em JS

--- arrays/array2.js
+++ arrays/array2.js
@@ -1,5 +1,14 @@
 const nomes = []; // Array vazio
-
+console.log(nomes);
 // Desafio: 
 // Inserir 7 nomes que existam na sala de aula no array
 // Imprimir os nomes no terminal/output
+nomes[0] = "Lucas"; 
+nomes[1] = "Gabriel"; 
+nomes[2] = "Joelson"; 
+nomes[3] = "Luiz"; 
+nomes[4] = "Rubens"; 
+nomes[5] = "Murilo"; 
+nomes[6] = "Vitor";
+
+console.log(nomes);
---
+++ arrays/metodos2.js
@@ -0,0 +1,7 @@
+// Índices       0  1  2  3  4  5
+const numeros = [1, 2, 3, 4, 5, 8];
+
+numeros.pop(); // Exclui o último valor
+console.log(numeros);
+numeros.splice(1, 2); // Remove o índice informado
+console.log(numeros);
